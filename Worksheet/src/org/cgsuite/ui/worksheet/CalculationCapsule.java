46fd2d069a074b4472b2fd6f3041098ae0c6c90c
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2011-02-24 02:33:49 +0000

    Factored out calculation capsule (towards multithreading)
    git-svn-id: svn://svn.code.sf.net/p/cgsuite/code/cgsuite/trunk@21 0138b134-6283-4d47-a013-9bc70bc83b06

---
+++ Worksheet/src/org/cgsuite/ui/worksheet/CalculationCapsule.java
@@ -0,0 +1,224 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cgsuite.ui.worksheet;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.Token;
+import org.cgsuite.lang.CgsuiteObject;
+import org.cgsuite.lang.CgsuitePackage;
+import org.cgsuite.lang.Domain;
+import org.cgsuite.lang.InputException;
+import org.cgsuite.lang.output.Output;
+import org.cgsuite.lang.output.StyledTextOutput;
+import org.cgsuite.lang.parser.CgsuiteLexer;
+import org.cgsuite.lang.parser.CgsuiteParser;
+import org.cgsuite.lang.parser.CgsuiteTree;
+import org.cgsuite.lang.parser.CgsuiteTreeAdaptor;
+
+/**
+ *
+ * @author asiegel
+ */
+public class CalculationCapsule implements Runnable
+{
+    private final static Domain WORKSPACE_DOMAIN = new Domain();
+    
+    private String text;
+    private Output[] output;
+    private boolean isErrorOutput;
+
+    public CalculationCapsule(String text)
+    {
+        this.text = text;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            CgsuitePackage.refreshAll();
+            ANTLRStringStream input = new ANTLRStringStream(text);
+            CgsuiteLexer lexer = new CgsuiteLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            CgsuiteParser parser = new CgsuiteParser(tokens);
+            parser.setTreeAdaptor(new CgsuiteTreeAdaptor());
+            CgsuiteParser.script_return r = parser.script();
+            CgsuiteTree tree = (CgsuiteTree) r.getTree();
+
+            if (parser.getNumberOfSyntaxErrors() > 0)
+            {
+                String firstErrorMsg = parser.getErrors().get(0).getMessage();
+                String[] linecol = firstErrorMsg.substring(5, firstErrorMsg.indexOf(' ', 5)).split(":");
+                int line = Integer.parseInt(linecol[0]);
+                int col = Integer.parseInt(linecol[1]);
+                output = getLineColOutput(text, line, col, "Syntax error.");
+                isErrorOutput = true;
+            }
+            else
+            {
+                output = new Output[] { invoke(tree) };
+            }
+        }
+        catch (Exception exc)
+        {
+            output = getExceptionOutput(text, exc, false);
+            isErrorOutput = true;
+        }
+    }
+
+    public Output[] getOutput()
+    {
+        return output;
+    }
+
+    public boolean isErrorOutput()
+    {
+        return isErrorOutput;
+    }
+
+    private Output invoke(final CgsuiteTree tree)
+    {
+        CgsuiteObject retval = WORKSPACE_DOMAIN.script(tree).invoke("Simplify");
+        return new CoreIOHandler().createOutput(retval);
+    }
+    
+    private static List<Output> getStackOutput(String input, InputException exc)
+    {
+        List<Output> output = new ArrayList<Output>();
+        output.add(errorOutput(exc.getMessage()));
+        if (exc.getCause() != null)
+        {
+            output.add(errorOutput("  caused by " + exc.getCause().getClass().getName()));
+            StackTraceElement[] javaStackTrace = exc.getCause().getStackTrace();
+            for (int i = 0; i < 3 && i < javaStackTrace.length; i++)
+            {
+                StackTraceElement ste = javaStackTrace[i];
+                output.add(errorOutput("  at " + ste.getClassName() + " line " + ste.getLineNumber() + "\n"));
+            }
+            if (javaStackTrace.length > 3)
+            {
+                output.add(errorOutput("  ......"));
+            }
+        }
+        for (Token token : exc.getTokenStack())
+        {
+            String source = token.getInputStream().getSourceName();
+            if (source == null)
+            {
+                output.add(errorOutput("  at worksheet input:\n"));
+                output.addAll(Arrays.asList(getLineColOutput(input, token.getLine(), token.getCharPositionInLine(), "")));
+            }
+            else
+            {
+                output.add(errorOutput(
+                    "  at " + source + " line " + token.getLine() + ":" + token.getCharPositionInLine() + "\n"
+                    ));
+            }
+        }
+        return output;
+    }
+
+    private static Output[] getExceptionOutput(String input, Exception exc, boolean includeLine)
+    {
+        int line = 0, col = 0;
+
+        if (exc instanceof RecognitionException)
+        {
+            line = ((RecognitionException) exc).line;
+            col  = ((RecognitionException) exc).charPositionInLine;
+        }
+        else if (exc instanceof InputException)
+        {
+            return getStackOutput(input, (InputException) exc).toArray(new Output[0]);
+        }
+        else
+        {
+            StringWriter sw = new StringWriter();
+            exc.printStackTrace(new PrintWriter(sw));
+            String[] strings = sw.toString().split("\n");
+            Output[] output = new Output[strings.length];
+            for (int i = 0; i < strings.length; i++)
+            {
+                output[i] = errorOutput(strings[i]);
+            }
+            return output;
+        }
+
+        String message =
+            (includeLine && line > 0 ? "Error (Line " + line + ":" + col + "): " : "")
+            + getMessageForException(exc);
+
+        if (input == null || line <= 0)
+        {
+            return new Output[] { new StyledTextOutput(message) };
+        }
+        else
+        {
+            return getLineColOutput(input, line, col, message);
+        }
+    }
+
+    private static Output[] getLineColOutput(String input, int line, int col, String message)
+    {
+        Output[] output = new Output[3];
+        // Advance to the point in the input string where the exceptional line begins.
+        int lineStartIndex = 0;
+        for (int i = 1; i < line; i++)
+        {
+            lineStartIndex = input.indexOf('\n', lineStartIndex) + 1;
+        }
+        int lineEndIndex = input.indexOf('\n', lineStartIndex);
+        // Next get the part of the input string that has the error.
+        output[0] = errorOutput(
+            "  " + input.substring(
+                Math.max(lineStartIndex, lineStartIndex + col - 24),
+                Math.min(lineEndIndex == -1 ? input.length() : lineEndIndex, lineStartIndex + col + 25)
+            ));
+        String pointerStr = "  ";
+        for (int i = 0; i < Math.min(col - 1, 22); i++)
+        {
+            pointerStr += " ";
+        }
+        output[1] = errorOutput(pointerStr + (col == 1 ? "^^" : "^^^"));
+        output[2] = new StyledTextOutput(message);
+        return output;
+    }
+
+    private static Output errorOutput(String msg)
+    {
+        return new StyledTextOutput(
+            EnumSet.of(StyledTextOutput.Style.FACE_MONOSPACED, StyledTextOutput.Style.COLOR_RED),
+            msg
+            );
+    }
+
+    private static String getMessageForException(Exception exc)
+    {
+        if (exc instanceof InputException)
+        {
+            return exc.getMessage();
+        }
+        else if (exc instanceof antlr.NoViableAltException &&
+                 ((antlr.NoViableAltException) exc).token.getType() == CgsuiteLexer.SLASHES)
+        {
+            return "Syntax error: Every slash | must be enclosed in braces { }.";
+        }
+        else
+        {
+            return "Syntax error.";
+        }
+    }
+
+}
--- Worksheet/src/org/cgsuite/ui/worksheet/WorksheetPanel.java
+++ Worksheet/src/org/cgsuite/ui/worksheet/WorksheetPanel.java
@@ -11,7 +11,6 @@
 
 package org.cgsuite.ui.worksheet;
 
-import org.cgsuite.lang.output.StyledTextOutput;
 import org.cgsuite.lang.output.Output;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -20,29 +19,12 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.List;
 import javax.swing.Box;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JViewport;
 import javax.swing.Scrollable;
-import org.antlr.runtime.ANTLRStringStream;
-import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.RecognitionException;
-import org.antlr.runtime.Token;
-import org.cgsuite.lang.parser.CgsuiteLexer;
-import org.cgsuite.lang.CgsuiteObject;
-import org.cgsuite.lang.CgsuitePackage;
-import org.cgsuite.lang.parser.CgsuiteParser;
-import org.cgsuite.lang.parser.CgsuiteTree;
-import org.cgsuite.lang.parser.CgsuiteTreeAdaptor;
-import org.cgsuite.lang.Domain;
-import org.cgsuite.lang.InputException;
+import org.openide.util.RequestProcessor;
 
 /**
  *
@@ -50,7 +32,7 @@ import org.cgsuite.lang.InputException;
  */
 public class WorksheetPanel extends javax.swing.JPanel implements Scrollable
 {
-    private final static Domain WORKSHEET_DOMAIN = new Domain();
+    private final static RequestProcessor REQUEST_PROCESSOR = new RequestProcessor(WorksheetPanel.class);
 
     /** Creates new form WorksheetPanel */
     public WorksheetPanel()
@@ -162,42 +144,11 @@ public class WorksheetPanel extends javax.swing.JPanel implements Scrollable
     
     private void processCommand(EmbeddedTextArea source)
     {
-        Output[] output = null;
-        CgsuiteTree tree = null;
-        try
-        {
-            CgsuitePackage.refreshAll();
-            ANTLRStringStream input = new ANTLRStringStream(source.getText());
-            CgsuiteLexer lexer = new CgsuiteLexer(input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            CgsuiteParser parser = new CgsuiteParser(tokens);
-            parser.setTreeAdaptor(new CgsuiteTreeAdaptor());
-            CgsuiteParser.script_return r = parser.script();
-            tree = (CgsuiteTree) r.getTree();
-
-            if (parser.getNumberOfSyntaxErrors() > 0)
-            {
-                String firstErrorMsg = parser.getErrors().get(0).getMessage();
-                String[] linecol = firstErrorMsg.substring(5, firstErrorMsg.indexOf(' ', 5)).split(":");
-                int line = Integer.parseInt(linecol[0]);
-                int col = Integer.parseInt(linecol[1]);
-                output = getLineColOutput(source.getText(), line, col, "Syntax error.");
-                getToolkit().beep();
-            }
-            else
-            {
-                CgsuiteObject retval = WORKSHEET_DOMAIN.script(tree);
-                output = new Output[]
-                {
-                    new CoreIOHandler().createOutput(retval.invoke("Simplify"))
-                };
-            }
-        }
-        catch (Exception exc)
-        {
-            output = getExceptionOutput(source.getText(), exc, false);
+        CalculationCapsule capsule = new CalculationCapsule(source.getText());
+        capsule.run();
+        Output[] output = capsule.getOutput();
+        if (capsule.isErrorOutput())
             getToolkit().beep();
-        }
 
         // Find this component.
         int index = findIndex(source, false) + 1;
@@ -313,133 +264,6 @@ public class WorksheetPanel extends javax.swing.JPanel implements Scrollable
             }
         }
         getParent().getParent().validate();
-    }
-
-    private static List<Output> getStackOutput(String input, InputException exc)
-    {
-        List<Output> output = new ArrayList<Output>();
-        output.add(errorOutput(exc.getMessage()));
-        if (exc.getCause() != null)
-        {
-            output.add(errorOutput("  caused by " + exc.getCause().getClass().getName()));
-            StackTraceElement[] javaStackTrace = exc.getCause().getStackTrace();
-            for (int i = 0; i < 3 && i < javaStackTrace.length; i++)
-            {
-                StackTraceElement ste = javaStackTrace[i];
-                output.add(errorOutput("  at " + ste.getClassName() + " line " + ste.getLineNumber() + "\n"));
-            }
-            if (javaStackTrace.length > 3)
-            {
-                output.add(errorOutput("  ......"));
-            }
-        }
-        for (Token token : exc.getTokenStack())
-        {
-            String source = token.getInputStream().getSourceName();
-            if (source == null)
-            {
-                output.add(errorOutput("  at worksheet input:\n"));
-                output.addAll(Arrays.asList(getLineColOutput(input, token.getLine(), token.getCharPositionInLine(), "")));
-            }
-            else
-            {
-                output.add(errorOutput(
-                    "  at " + source + " line " + token.getLine() + ":" + token.getCharPositionInLine() + "\n"
-                    ));
-            }
-        }
-        return output;
-    }
-
-    private static Output[] getExceptionOutput(String input, Exception exc, boolean includeLine)
-    {
-        int line = 0, col = 0;
-
-        if (exc instanceof RecognitionException)
-        {
-            line = ((RecognitionException) exc).line;
-            col  = ((RecognitionException) exc).charPositionInLine;
-        }
-        else if (exc instanceof InputException)
-        {
-            return getStackOutput(input, (InputException) exc).toArray(new Output[0]);
-        }
-        else
-        {
-            StringWriter sw = new StringWriter();
-            exc.printStackTrace(new PrintWriter(sw));
-            String[] strings = sw.toString().split("\n");
-            Output[] output = new Output[strings.length];
-            for (int i = 0; i < strings.length; i++)
-            {
-                output[i] = errorOutput(strings[i]);
-            }
-            return output;
-        }
-
-        String message =
-            (includeLine && line > 0 ? "Error (Line " + line + ":" + col + "): " : "")
-            + getMessageForException(exc);
-
-        if (input == null || line <= 0)
-        {
-            return new Output[] { new StyledTextOutput(message) };
-        }
-        else
-        {
-            return getLineColOutput(input, line, col, message);
-        }
-    }
-
-    private static Output[] getLineColOutput(String input, int line, int col, String message)
-    {
-        Output[] output = new Output[3];
-        // Advance to the point in the input string where the exceptional line begins.
-        int lineStartIndex = 0;
-        for (int i = 1; i < line; i++)
-        {
-            lineStartIndex = input.indexOf('\n', lineStartIndex) + 1;
-        }
-        int lineEndIndex = input.indexOf('\n', lineStartIndex);
-        // Next get the part of the input string that has the error.
-        output[0] = errorOutput(
-            "  " + input.substring(
-                Math.max(lineStartIndex, lineStartIndex + col - 24),
-                Math.min(lineEndIndex == -1 ? input.length() : lineEndIndex, lineStartIndex + col + 25)
-            ));
-        String pointerStr = "  ";
-        for (int i = 0; i < Math.min(col - 1, 22); i++)
-        {
-            pointerStr += " ";
-        }
-        output[1] = errorOutput(pointerStr + (col == 1 ? "^^" : "^^^"));
-        output[2] = new StyledTextOutput(message);
-        return output;
-    }
-
-    private static Output errorOutput(String msg)
-    {
-        return new StyledTextOutput(
-            EnumSet.of(StyledTextOutput.Style.FACE_MONOSPACED, StyledTextOutput.Style.COLOR_RED),
-            msg
-            );
-    }
-
-    private static String getMessageForException(Exception exc)
-    {
-        if (exc instanceof InputException)
-        {
-            return exc.getMessage();
-        }
-        else if (exc instanceof antlr.NoViableAltException &&
-                 ((antlr.NoViableAltException) exc).token.getType() == CgsuiteLexer.SLASHES)
-        {
-            return "Syntax error: Every slash | must be enclosed in braces { }.";
-        }
-        else
-        {
-            return "Syntax error.";
-        }
     }
     
     @Override
