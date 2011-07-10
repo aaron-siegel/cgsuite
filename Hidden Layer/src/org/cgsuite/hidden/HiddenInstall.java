8eceb853eddd3db003d917463bdac5066358bd47
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2011-07-10 17:29:29 +0000

    project installer (for ui tweaks etc)
    git-svn-id: svn://svn.code.sf.net/p/cgsuite/code/cgsuite/trunk@68 0138b134-6283-4d47-a013-9bc70bc83b06

---
+++ Hidden Layer/src/org/cgsuite/hidden/HiddenInstall.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cgsuite.hidden;
+
+import org.openide.modules.ModuleInstall;
+
+/**
+ *
+ * @author asiegel
+ */
+public class HiddenInstall extends ModuleInstall
+{
+    @Override
+    public void restored()
+    {
+    }
+}
