a5d5c7c3ddea6ea20f81f851112bcc7d4e01af90
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2011-07-26 16:55:47 +0000

    import help system from 0.7
    git-svn-id: svn://svn.code.sf.net/p/cgsuite/code/cgsuite/trunk@115 0138b134-6283-4d47-a013-9bc70bc83b06

---
+++ Help/src/org/cgsuite/help/docs/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "help-hs.xml", position = 3652)
+package org.cgsuite.help.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
