40ca5e44085d2703eb9d65f7ed2e451a7d1828e9
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2020-11-19 16:51:02 -0800

    Remove dependency on lang package

--- lib/core/src/main/scala/org/cgsuite/lang2/parser/ParserUtil.scala
+++ lib/core/src/main/scala/org/cgsuite/lang2/parser/ParserUtil.scala
@@ -2,10 +2,12 @@ package org.cgsuite.lang2.parser
 
 import java.io.{ByteArrayInputStream, InputStream}
 
+import org.antlr.runtime.tree.Tree
 import org.antlr.runtime.{CharStream, CommonTokenStream}
+import org.cgsuite.exception.SyntaxException
 import org.cgsuite.lang.parser.{CgsuiteLexer, CgsuiteParser}
-import org.cgsuite.lang.parser.ParserUtil.parse
 
+import scala.collection.JavaConversions._
 import scala.language.reflectiveCalls
 
 object ParserUtil {
@@ -22,6 +24,28 @@ object ParserUtil {
     val parser = new CgsuiteParser(tokens)
     parser.setTreeAdaptor(new CgsuiteTreeAdaptor(tokens))
     parser
+
+  }
+
+  private[cgsuite] def parse(in: InputStream, source: String)(fn: CgsuiteParser => { def getTree(): Object }): Tree = {
+
+    val stream = new SourcedAntlrInputStream(in, source)
+    val lexer = new CgsuiteLexer(stream)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new CgsuiteParser(tokens)
+    parser.setTreeAdaptor(new CgsuiteTreeAdaptor(tokens))
+
+    val tree = fn(parser).getTree().asInstanceOf[CgsuiteTree]
+
+    if (!lexer.getErrors.isEmpty) {
+      throw SyntaxException(lexer.getErrors.head.getException, source)
+      //throw InputException("Syntax error: " + lexer.getErrors.get(0).getMessage)
+    } else if (!parser.getErrors.isEmpty) {
+      throw SyntaxException(parser.getErrors.head.getException, source)
+      //s"Syntax error at ${err.getException.token.getInputStream.getSourceName} line ${err.getException.line}:${err.getException.charPositionInLine}: ${err.getMessage}"
+    } else {
+      tree
+    }
 
   }
 
