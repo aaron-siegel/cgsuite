c90e1e5d60b8577dcdcc8e8207c5c69b34b1853c
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2021-09-22 21:23:35 -0700

    Fix repl.sh

--- lib/core/repl.sh
+++ lib/core/repl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Dorg.cgsuite.devbuild=.. -cp target/classes:target/cgsuite-core-2.0.0-SNAPSHOT-jar-with-dependencies.jar org.cgsuite.lang.Repl
+java -Dorg.cgsuite.devbuild=.. -cp target/cgsuite-core-2.0.0-SNAPSHOT-jar-with-dependencies.jar org.cgsuite.lang.Repl
