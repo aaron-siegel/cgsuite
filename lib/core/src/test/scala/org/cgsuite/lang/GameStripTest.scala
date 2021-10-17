71cf1a599cdc77e0a062a67f0c9b860cf1ad0656
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2021-10-17 12:54:39 -0700

    Strip games unit tests

--- lib/core/src/test/scala/org/cgsuite/lang/GameStripTest.scala
+++ lib/core/src/test/scala/org/cgsuite/lang/GameStripTest.scala
@@ -9,7 +9,15 @@ class GameStripTest extends CgscriptSpec {
       header,
       ("ToadsAndFrogs", """game.strip.ToadsAndFrogs("ttttt..fffff").CanonicalForm""", "+-{{2|*},{5/2||2|{0||||{0||v<2>|-1},{0||||0||Miny(1/32)|-2|||-1/2*}|||v<2>|-1/2||-1*}|||0}}"),
       ("BackslidingToadsAndFrogs", """game.strip.BackslidingToadsAndFrogs("ttt..fff").GameValue""", "{on||0|-1/2} & {1/2|0||off}"),
-      ("GenToadsAndFrogs", """game.strip.GenToadsAndFrogs(2)("tttt..fff").CanonicalForm""", "{1/2*|v}")
+      ("GenToadsAndFrogs", """game.strip.GenToadsAndFrogs(2)("tttt..fff").CanonicalForm""", "{1/2*|v}"),
+      ("ElephantsAndRhinos", """game.strip.ElephantsAndRhinos("tttt..fff").CanonicalForm""", "1")
+    ))
+  }
+
+  "game.strip" should "define Turning properly" in {
+    executeTests(Table(
+      header,
+      ("Turning", """game.strip.Turning(game.heap.Spawning("1-3"))("htththtth").NimValue""", "29")
     ))
   }
 
