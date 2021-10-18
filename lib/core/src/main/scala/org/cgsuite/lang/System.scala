94487d88dbdc455deb78408c57299728dade7b2f
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2021-10-18 15:34:10 -0700

    Step version to beta3

--- lib/core/src/main/scala/org/cgsuite/lang/System.scala
+++ lib/core/src/main/scala/org/cgsuite/lang/System.scala
@@ -19,7 +19,7 @@ object System extends LazyLogging {
 
   val copyrightYear = "2003-2021"
 
-  val version = "2.0-beta2"
+  val version = "2.0-beta3"
 
   def clearAll(): Unit = {
     UiHarness.uiHarness.clearUiVars()
