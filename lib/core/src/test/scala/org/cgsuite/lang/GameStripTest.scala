e4d0e4b0a0c3d9b0e1cd4842a14172bf88005fec
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2021-10-19 16:14:57 -0700

    Flesh out GenToadsAndFrogs

--- lib/core/src/test/scala/org/cgsuite/lang/GameStripTest.scala
+++ lib/core/src/test/scala/org/cgsuite/lang/GameStripTest.scala
@@ -9,7 +9,7 @@ class GameStripTest extends CgscriptSpec {
       header,
       ("ToadsAndFrogs", """game.strip.ToadsAndFrogs("ttttt..fffff").CanonicalForm""", "+-{{2|*},{5/2||2|{0||||{0||v<2>|-1},{0||||0||Miny(1/32)|-2|||-1/2*}|||v<2>|-1/2||-1*}|||0}}"),
       ("BackslidingToadsAndFrogs", """game.strip.BackslidingToadsAndFrogs("ttt..fff").GameValue""", "{on||0|-1/2} & {1/2|0||off}"),
-      ("GenToadsAndFrogs", """game.strip.GenToadsAndFrogs(2)("tttt..fff").CanonicalForm""", "{1/2*|v}"),
+      ("GenToadsAndFrogs", """game.strip.GenToadsAndFrogs(maxJump => 2)("tttt..fff").CanonicalForm""", "{1/2*|v}"),
       ("ElephantsAndRhinos", """game.strip.ElephantsAndRhinos("tttt..fff").CanonicalForm""", "1")
     ))
   }
