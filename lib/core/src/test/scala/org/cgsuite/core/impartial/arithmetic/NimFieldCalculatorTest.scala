fe77f874cbbf6021aa66b6100ed6002c6d43b191
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2023-04-28 21:19:11 -0700

    Slight refactor of NimFieldCalculatorTest

--- lib/core/src/test/scala/org/cgsuite/core/impartial/arithmetic/NimFieldCalculatorTest.scala
+++ lib/core/src/test/scala/org/cgsuite/core/impartial/arithmetic/NimFieldCalculatorTest.scala
@@ -5,14 +5,14 @@ import org.scalatest.matchers.should.Matchers
 
 class NimFieldCalculatorTest extends AnyFlatSpec with Matchers {
 
-  def checkCalculations(index: Int): Unit = {
-    val p = NimFieldConstants.primes(index)
+  def checkCalculations(p: Int): Unit = {
+    val index = NimFieldConstants.primes.indexOf(p)
     NimFieldCalculator.qSet(p) shouldBe NimFieldConstants.qSet(index)
     NimFieldCalculator.excess(p) shouldBe NimFieldConstants.excess(index)
   }
 
   "NimFieldCalculator" should "compute excess and Q-sets correctly for p <= 43" in {
-    0 to NimFieldConstants.primes.indexOf(43) foreach checkCalculations
+    NimFieldConstants.primes takeWhile { _ <= 43 } foreach checkCalculations
   }
 
   it should "compute excess and Q-set correctly for p = 229" in {
@@ -20,7 +20,7 @@ class NimFieldCalculatorTest extends AnyFlatSpec with Matchers {
     // which the excess is > 1. The case p = 191 also has this property, but
     // takes longer to compute.)
     NimFieldCalculator.preloadTo(227)
-    checkCalculations(NimFieldConstants.primes.indexOf(229))
+    checkCalculations(229)
   }
 
 }
